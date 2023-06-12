c38f5de6cc5cb929a82f5396a76fa886c4bdc273
alperencngz <cengiz.ozturk@ozu.edu.tr>
2023-06-12 08:45:01 +0300

    Added keystroke validation
    reached the form logic we want, now lets clear the code

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -15,6 +15,10 @@ const SimpleInput = (props) => {
 
   const nameInputChangeHandler = event => {
     setEnteredName(event.target.value);
+
+    if (event.target.value.trim() !== "") {
+      setEnteredNameIsValid(true);
+    }
   };
 
   const nameInputBlurHandler = (event) => {
@@ -22,7 +26,6 @@ const SimpleInput = (props) => {
 
     if (enteredName.trim() === "") {
       setEnteredNameIsValid(false);
-      return;
     }
   }
 
