8eb20df9320b0dce60e91817c6d56559c079c4eb
alperencngz <cengiz.ozturk@ozu.edu.tr>
2023-06-12 13:20:46 +0300

    useReducer option for custom hook

--- src/hooks/use-input-2.js
+++ src/hooks/use-input-2.js
@@ -1,35 +1,52 @@
-import { useState } from "react"
+import { useReducer } from 'react';
+
+const initialInputState = {
+  value: '',
+  isTouched: false,
+};
+
+const inputStateReducer = (state, action) => {
+  if (action.type === 'INPUT') {
+    return { value: action.value, isTouched: state.isTouched };
+  }
+  if (action.type === 'BLUR') {
+    return { isTouched: true, value: state.value };
+  }
+  if (action.type === 'RESET') {
+    return { isTouched: false, value: '' };
+  }
+  return state;
+};
 
 const useInput = (validateValue) => {
+  const [inputState, dispatch] = useReducer(
+    inputStateReducer,
+    initialInputState
+  );
 
-    const [enteredValue, setEnteredValue] = useState("");
-    const [isTouched, setIsTouched] = useState(false);
+  const valueIsValid = validateValue(inputState.value);
+  const hasError = !valueIsValid && inputState.isTouched;
 
-    const valueIsValid = validateValue(enteredValue);
-    const hasError = !valueIsValid && isTouched;
+  const valueChangeHandler = (event) => {
+    dispatch({ type: 'INPUT', value: event.target.value });
+  };
 
-    const valueChangeHandler = (event) => {
-        setEnteredValue(event.target.value);
-    }
+  const inputBlurHandler = (event) => {
+    dispatch({ type: 'BLUR' });
+  };
 
-    const inputBlurHandler = (event) => {
-        setIsTouched(true);
-    }
+  const reset = () => {
+    dispatch({ type: 'RESET' });
+  };
 
-    const reset = () => {
-        setIsTouched(false);
-        setEnteredValue("");
-    }
-
-    return {
-        value: enteredValue,
-        isValid: valueIsValid,
-        hasError: hasError,
-        valueChangeHandler,
-        inputBlurHandler,
-        reset
-    }
-
-}
+  return {
+    value: inputState.value,
+    isValid: valueIsValid,
+    hasError,
+    valueChangeHandler,
+    inputBlurHandler,
+    reset,
+  };
+};
 
 export default useInput;
